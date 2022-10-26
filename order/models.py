76f9bca82fd307f882ae2df06fde2951832e7cdc
newsoft111 <ydj0617@gmail.com>
2022-10-26 15:02:45 +0900

    z

---
+++ order/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+import uuid
+from product.models import *
+from account.models import UserShippingAddress
+
+# Create your models here.
+class Order(models.Model):
+	user = models.ForeignKey(
+			settings.AUTH_USER_MODEL,
+			on_delete=models.CASCADE,
+	)
+	pg_uid = models.CharField(max_length=255, null=True)
+	ordered_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+	shpping_address = models.ForeignKey(
+			UserShippingAddress,
+			on_delete=models.CASCADE,
+			null=True
+	)
+	is_paid=models.BooleanField(default=False)
+	paid_at = models.DateTimeField(null=True)
+
+	class Meta:
+		db_table = 'ecommerce_order'
+
+
+class OrderItem(models.Model):
+	order = models.ForeignKey(
+			Order,
+			on_delete=models.CASCADE
+	)
+	order_uid = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True, editable=False)
+	is_cancelled=models.BooleanField(default=False)
+	cancelled_at = models.DateTimeField(null=True)
+	is_refunded=models.BooleanField(default=False)
+	refunded_at = models.DateTimeField(null=True)
+
+	product = models.ForeignKey(
+			Product,
+			on_delete=models.CASCADE,
+	)
+	product_name = models.CharField(max_length=255)
+	product_price = models.PositiveIntegerField()
+	variant = models.CharField(max_length=255, null=True)
+	variant_value = models.CharField(max_length=255, null=True)
+	variant_price = models.PositiveIntegerField(null=True)
+	ordered_quantity = models.PositiveIntegerField()
+	shipped_quantity = models.PositiveIntegerField()
+	
+
+	class Meta:
+		db_table = 'ecommerce_order_item'
+
+	def sub_total_price(self):
+		if self.variant is not None:
+			return (self.product_price + self.variant_price) * self.ordered_quantity
+		else:
+			return self.product_price * self.ordered_quantity
---
+++ product/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import *
+from django.core.paginator import Paginator
+# Create your views here.
+
+@login_required(login_url="acount:login")
+def product_list(request):
+	seo = {
+		'title': "상품 리스트 - 유토빌",
+	}
+	product_list =  Product.objects.all().order_by( "-id")
+	
+	page        = int(request.GET.get('p', 1))
+	pagenator   = Paginator(product_list, 12)
+	product_list = pagenator.get_page(page)
+
+	return render(request, 'product/product_list.html',{
+		"seo":seo,
+		'product_list': product_list
+	})
+	
