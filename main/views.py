e8ab3f00874c5aff6b050143f2474593200ab2bb
newsoft111 <ydj0617@gmail.com>
2022-10-27 13:49:32 +0900

    ㅋ

--- main/views.py
+++ main/views.py
@@ -6,7 +6,4 @@ def index(request):
 	seo = {
 		'title': "유토빌",
 	}
-	if request.user.is_authenticated: #로그인 상태면
-		return render(request, 'main/index.html',{"seo":seo})
-	else:
-		return redirect("account:login")
+	return render(request, 'main/index.html',{"seo":seo})
