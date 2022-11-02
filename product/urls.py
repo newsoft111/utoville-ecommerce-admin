712f865dfb52e45552fe663982a2b1b0a70d8446
deepakdubey <deepakdubey@goodearth.in>
2022-11-02 16:36:11 +0530

    category dev done

--- product/urls.py
+++ product/urls.py
@@ -6,5 +6,6 @@ app_name = 'product'
 urlpatterns = [
 	path('', views.product_list, name='list'),
 	path('write/', views.product_write, name='write'),
+	path('l2_l3_category/', views.L2L3Category, name='l2_l3_category'),
 	path('upload/image', views.product_upload_content_image, name='upload_image'),
 ]
