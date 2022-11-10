82cd339feb861913aa4521cc7b1d66e1ef836f57
newsoft111 <ydj0617@gmail.com>
2022-11-10 11:34:30 +0900

    ㄹ

--- revenue/views.py
+++ revenue/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import *
-from django.db.models import Q
-from datetime import datetime
+from django.db.models import Q, Sum
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 def revenue_list(request):
-	start_date = datetime.now()-relativedelta(months=1)
-	end_date = datetime.now()
+	now = date.today()
+	start_date = now-relativedelta(months=1)
+	end_date = now
 
 	if request.GET.get("start_date"):
 		start_date = datetime.strptime(request.GET.get("start_date"), "%Y-%m-%d")
@@ -15,6 +16,28 @@ def revenue_list(request):
 		
 
 	revenue_admin_objs = RevenueAdmin.objects.filter(date__range = [start_date, end_date])
+	payment_amount = revenue_admin_objs.aggregate(Sum('payment_amount'))['payment_amount__sum']
+	refund_amount = revenue_admin_objs.aggregate(Sum('refund_amount'))['refund_amount__sum']
+	total_payment = payment_amount-refund_amount
+
+
+	temp = { po.date: {"payment_amount": po.payment_amount, "refunt_amount": po.payment_amount, "order_count":po.order_count} for po in revenue_admin_objs }
+
+
+	result = []
+
+	for dy in range(0, (end_date-start_date).days):
+		loop_date = (now-timedelta(days=dy))
+		result.append({
+			'date': loop_date,
+			'payment_amount': temp[loop_date]['payment_amount'] if loop_date in temp else 0,
+			'refunt_amount': temp[loop_date]['refunt_amount'] if loop_date in temp else 0,
+			'order_count': temp[loop_date]['order_count'] if loop_date in temp else 0,
+		})
+
 	return render(request, 'revenue/revenue_list.html', {
-		"revenue_admin_objs": revenue_admin_objs
+		"revenue_admin_objs": result,
+		'payment_amount': payment_amount,
+		'refund_amount': refund_amount,
+		'total_payment': total_payment
 	})
