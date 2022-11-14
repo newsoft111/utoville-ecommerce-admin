54088c2317ffa390562a200b3a34b0f819fb6060
deepakdubey <deepakdubey@goodearth.in>
2022-11-14 17:55:39 +0530

    product update dev done

--- config/settings.py
+++ config/settings.py
@@ -83,6 +83,7 @@ TEMPLATES = [
 
 			'libraries':{
             	'util_filter': 'util.templatetags.util_filter',
+            	'product_filters': 'product.templatetags.product_filters',
             }
         },
     },
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include       #new
+from . import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
 	path('', include('main.urls')),
@@ -9,4 +11,4 @@ urlpatterns = [
 	path('qna/', include('qna.urls')),
 	path('profit/', include('profit.urls')),
 	path('revenue/', include('revenue.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
